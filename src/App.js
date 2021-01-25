d4a722ea35457d3ffc2fd3fa2fe8161dccaa8c67
keyurparalkar <keur.plkar@gmail.com>
2021-01-25 13:42:12 +0530

    Implemented Lazy loading for card container

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
-import { useEffect, useState } from 'react';
 import tw from 'twin.macro';
+import React, { Suspense, useEffect} from 'react';
 
 import './tailwind.css';
-import Card from './Components/Card';
-import CardContainer from "./Components/CardContainer";
 import Header from './Components/Header';
-import { getRandomPhotos } from "./services/api.services";
+import { Spinner } from "./Components/Loaders/Spinner.loader";
+const CardContainer = React.lazy(() => import("./Components/CardContainer"));
 
-const StyledContainer = tw.div`font-sans antialiased`;
+const StyledContainer = tw.div`flex font-sans antialiased`;
 
 function App() {
-  const [photoData, setPhotoData] = useState(null);
-
   useEffect(() => {
     document.body.style.backgroundColor = "#f5f5f5";
-    
+  }, [])  
 
-    const getData = async () => {
-      const data = await getRandomPhotos();
-      setPhotoData(data.data);
-    }
-
-    getData();
-  }, [])
-
-  const listPhotoData = photoData ? photoData.map(({ urls ,downloads, likes, alt_description, user}) => {
-    return <Card
-            imgUrl = {urls.regular}
-            downloads = {downloads}
-            likes = {likes}
-            alt_description = {alt_description}
-            name = {user.name}
-      />
-  }) : '';
-  
   return (
     <StyledContainer>
       <Header/>
-      <CardContainer>
-              {listPhotoData}
-        </CardContainer>
+      <Suspense fallback={<Spinner/>}>
+          <CardContainer/>
+      </Suspense>
     </StyledContainer>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import tw from 'twin.macro';
+
 import TagButton from "./TagButton";
 import DownloadIcon from "./Icons/download.icon";
 import LikeIcon from "./Icons/like.icon";
 
 const StyledCard = tw.div`lg:w-1/5 md:w-full sm:w-full my-2 bg-white rounded-lg overflow-hidden border shadow`;
 
-const tags = ["man", "drinking", "coffee"].map((val)=> <TagButton>{val}</TagButton>);
+const tags = ["man", "drinking", "coffee"].map((val) => <TagButton>{val}</TagButton>);
 
 const Card = (props) => (
     <StyledCard>
         <img tw="w-full" alt='...' src={props.imgUrl} />
-
         <div tw="p-6">
             <div id="downloads_likes" tw="flex flex-row text-gray-600 text-sm pb-2 font-semibold">
                 <div tw="flex flex-row">
-                    <DownloadIcon/>&nbsp;{props.downloads}
+                    <DownloadIcon />&nbsp;{props.downloads}
                 </div> &nbsp; &bull; &nbsp;
                 <div tw="flex flex-row">
-                    <LikeIcon/>&nbsp;{props.likes}
+                    <LikeIcon />&nbsp;{props.likes}
                 </div>
             </div>
             <div id="description"><h4 tw="font-semibold my-0 pb-1 text-lg">{props.alt_description}</h4></div>
             <div id="author" tw="my-0 text-gray-600 hover:underline">
                 {props.name}
-                </div>
+            </div>
             <div id="tags" tw="mt-4">{tags}</div>
         </div>
     </StyledCard>
